3e48fbdaa270ea9aec250ff75692bb19e07c74cd
mrmlnc <mrmlnc@yandex-team.ru>
2019-05-05 22:28:48 +0300

    refactor: drop unused packages

--- windows/choco/todoist/setup.ps1
+++
@@ -1,5 +0,0 @@
-Import-Module -Name "$PSScriptRoot\..\..\utils.ps1" -Force
-
-[Logger]::Setup("Todoist")
-
-choco install todoist
--- windows/choco/xmind/setup.ps1
+++
@@ -1,5 +0,0 @@
-Import-Module -Name "$PSScriptRoot\..\..\utils.ps1" -Force
-
-[Logger]::Setup("XMind")
-
-choco install xmind
